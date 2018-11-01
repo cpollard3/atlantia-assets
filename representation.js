51a6d6ef0935214f3f8e31ad7224f0e6f2c4d169
Pollard, Christopher <cpollard@lgsinnovations.com>
2018-11-01 08:34:22 -0600

    Initial commit

---
+++ representation.js
@@ -0,0 +1,499 @@
+ATL.Premade = {
+  "Tree": [
+    {
+      "name": "Pine Tree",
+      "geometryURL": "Environment/Tree/pine-tree.json",
+      "textureURL": "Environment/Tree/pine-tree.svg",
+      "defaults": {
+        "Bark_Color": "#d35400",
+        "Leaf_Color": "#27ae60"
+      },
+      "createdBy": "Chris Pollard",
+      "license": "CC0",
+      'website': "http://atlantia.tv"
+    },{
+      "name": "Oak Tree",
+      "geometryURL": "Environment/Tree/oak-tree.json",
+      "textureURL": "Environment/Tree/pine-tree.svg",
+      "defaults": {
+        "Bark_Color": "#d35400",
+        "Leaf_Color": "#27ae60"
+      },
+      "createdBy": "Chris Pollard",
+      "license": "CC0",
+      'website': "http://atlantia.tv"
+    },{
+      "name": "Palm Tree",
+      "geometryURL": "Environment/Tree/palm-tree.json",
+      "textureURL": "Environment/Tree/pine-tree.svg",
+      "defaults": {
+        "Bark_Color": "#d35400",
+        "Leaf_Color": "#27ae60"
+      },
+      "createdBy": "Chris Pollard",
+      "license": "CC0",
+      'website': "http://atlantia.tv"
+    }],
+  "Flower": [
+    {
+      "name": "Daisy",
+      "geometryURL": "Environment/Flower/daisy.json",
+      "textureURL": "Environment/Flower/flower.svg",
+      "defaults": {
+        "Inside_Color": "#f1c40f",
+        "Petal_Color": "#9b59b6",
+        "Stem_Color": "#27ae60",
+      },
+      "createdBy": "Chris Pollard",
+      "license": "CC0",
+      'website': "http://atlantia.tv"
+    }
+  ],
+  "Mushroom": [
+    {
+      "name": "Toad Stool",
+      "geometryURL": "Environment/Mushroom/toad-stool.json",
+      "textureURL": "Environment/Mushroom/toad-stool.svg",
+      "defaults": {
+        "Cap_Color": "#e74c3c",
+        "Spot_Color": "#ecf0f1",
+        "Stem_Color": "#ecf0f1",
+      },
+      "createdBy": "Chris Pollard",
+      "license": "CC0",
+      'website': "http://atlantia.tv"
+    }
+  ],
+  "Bench": [
+    {
+      "name": "Rustic",
+      "geometryURL": "Furniture/Bench/rustic.json",
+      "textureURL": "Furniture/Bench/rustic.svg",
+      "size": 256,
+      "defaults": {
+        "Bark_Color": "#e74c3c",
+        "Wood_Color": "#e67e22",
+      },
+      "createdBy": "Chris Pollard",
+      "license": "CC0",
+      'website': "http://atlantia.tv"
+    }
+  ],
+  "Crystal": [
+    {
+      "name": "Crystal A",
+      "geometryURL": "Environment/Crystal/crystal-a.json",
+      "textureURL": "Environment/Crystal/crystal-a.svg",
+      "defaults": {
+        "Crystal_Color": "#83F52C"
+      },
+      "createdBy": "Chris Pollard",
+      "license": "CC0",
+      'website': "http://atlantia.tv"
+    }
+  ],
+  "Crate": [
+    {
+      "name": "Crate A",
+      "geometryURL": "Furniture/Crate/crate-a.json",
+      "textureURL": "Furniture/Crate/crate-a.svg",
+      "defaults": {
+        "Face_Color": "#e67e22",
+        "Wood_Color": "#f39c12",
+      },
+      "createdBy": "Chris Pollard",
+      'website': "http://atlantia.tv"
+    }
+  ],
+  "Boat": [
+    {
+      "name": "Sail Boat",
+      "geometryURL": "Vehicles/Boat/sail-boat.json",
+      "textureURL": "Vehicles/Boat/sail-boat.svg",
+      "defaults": {
+        "Hull_Color": "#e74c3c",
+        "Wood_Color": "#e67e22",
+        "Sail_Color": "#ecf0f1",
+      },
+      "createdBy": "Chris Pollard",
+      "license": "CC0",
+      'website': "http://atlantia.tv"
+    }
+  ],
+  "Hat": [
+    {
+      "name": "Crown",
+      "geometryURL": "Accessories/Hat/crown.json",
+      "textureURL": "Accessories/Hat/crown.svg",
+      "defaults": {
+        "Cap_Color": "#f1c40f",
+        "Gem_Color": "#e74c3c",
+      },
+      "createdBy": "Chris Pollard",
+      "license": "CC0",
+      'website': "http://atlantia.tv"
+    },
+    {
+      "name": "Santa",
+      "geometryURL": "Accessories/Hat/santa.json",
+      "textureURL": "Accessories/Hat/graduation.svg",
+      "defaults": {
+        "Cap_Color": "#e74c3c",
+        "Tassle_Color": "#ecf0f1",
+      },
+      "createdBy": "Lucian-Pavel-728 (modified)",
+      "license": "CC0",
+      'website': "http://opengameart.org/users/lucian-pavel-728"
+    },
+    {
+      "name": "Top Hat",
+      "geometryURL": "Accessories/Hat/top.json",
+      "textureURL": "Accessories/Hat/top.svg",
+      "defaults": {
+        "Cap_Color": "#2c3e50",
+        "Brim_Color": "#ecf0f1",
+      },
+      "createdBy": "johndh (modified)",
+      "license": "CC-BY-SA 3.0",
+      'website': "http://opengameart.org/users/johndh"
+    },
+    {
+      "name": "Graduation",
+      "geometryURL": "Accessories/Hat/graduation.json",
+      "textureURL": "Accessories/Hat/graduation.svg",
+      "defaults": {
+        "Cap_Color": "#2c3e50",
+        "Tassle_Color": "#f1c40f",
+      },
+      "createdBy": "Chris Pollard",
+      "license": "CC0",
+      'website': "http://atlantia.tv"
+    }
+  ],
+  "Bridge": [
+    {
+      "name": "Rustic",
+      "geometryURL": "Buildings/Bridge/simple.json",
+      "textureURL": "Buildings/Bridge/simple.svg",
+      "defaults": {
+        "Wood_Color": "#e67e22",
+        "Bark_Color": "#e74c3c",
+      },
+      "createdBy": "Chris Pollard",
+      "license": "CC0",
+      'website': "http://atlantia.tv"
+    }
+  ],
+  "Chair": [
+    {
+      "name": "Basic",
+      "geometryURL": "Furniture/Chair/simple-chair.json",
+      "textureURL": "Furniture/Chair/simple.svg",
+      "defaults": {
+        "Wood_Color": "#f39c12",
+        "Face_Color": "#e67e22",
+      },
+      "createdBy": "arikel (modified)",
+      "license": "CC0",
+      'website': "http://opengameart.org/users/arikel"
+    }
+  ],
+  "Table": [
+    {
+      "name": "Basic",
+      "geometryURL": "Furniture/Table/simple-table.json",
+      "textureURL": "Furniture/Table/simple.svg",
+      "defaults": {
+        "Wood_Color": "#f39c12",
+        "Face_Color": "#e67e22",
+      },
+      "createdBy": "arikel (modified)",
+      "license": "CC0",
+      'website': "http://opengameart.org/users/arikel"
+    }
+  ],
+  "Bookcase": [
+    {
+      "name": "Basic",
+      "geometryURL": "Furniture/Bookcase/simple-bookcase.json",
+      "textureURL": "Furniture/Bookcase/simple.svg",
+      "defaults": {
+        "Wood_Color": "#f39c12",
+        "Face_Color": "#e67e22",
+      },
+      "createdBy": "arikel (modified)",
+      "license": "CC0",
+      'website': "http://opengameart.org/users/arikel"
+    }
+  ],
+  "Side Table": [
+    {
+      "name": "Basic",
+      "geometryURL": "Furniture/SideTable/side-table.json",
+      "textureURL": "Furniture/SideTable/simple.svg",
+      "defaults": {
+        "Wood_Color": "#f39c12",
+        "Face_Color": "#e67e22",
+      },
+      "createdBy": "arikel (modified)",
+      "license": "CC0",
+      'website': "http://opengameart.org/users/arikel"
+    }
+  ],
+  "Cup": [
+    {
+      "name": "Basic",
+      "geometryURL": "Items/Dining/cup.json",
+      "textureURL": "Items/Dining/simple.svg",
+      "defaults": {
+        "Color": "#95a5a6",
+      },
+      "createdBy": "Clint Bellanger (modified)",
+      "license": "CC-BY 3.0",
+      'website': "http://opengameart.org/content/dinnerware"
+    }
+  ],
+  "Bowl": [
+    {
+      "name": "Basic",
+      "geometryURL": "Items/Dining/dinnerbowl.json",
+      "textureURL": "Items/Dining/simple.svg",
+      "defaults": {
+        "Color": "#d35400",
+      },
+      "createdBy": "Clint Bellanger (modified)",
+      "license": "CC-BY 3.0",
+      'website': "http://opengameart.org/content/dinnerware"
+    }
+  ],
+  "Plate": [
+    {
+      "name": "Basic",
+      "geometryURL": "Items/Dining/dinnerplate.json",
+      "textureURL": "Items/Dining/simple.svg",
+      "defaults": {
+        "Color": "#ecf0f1",
+      },
+      "createdBy": "Clint Bellanger (modified)",
+      "license": "CC-BY 3.0",
+      'website': "http://opengameart.org/content/dinnerware"
+    }
+  ],
+  "Fork": [
+    {
+      "name": "Basic",
+      "geometryURL": "Items/Dining/fork.json",
+      "textureURL": "Items/Dining/simple.svg",
+      "defaults": {
+        "Color": "#95a5a6",
+      },
+      "createdBy": "Clint Bellanger (modified)",
+      "license": "CC-BY 3.0",
+      'website': "http://opengameart.org/content/dinnerware"
+    }
+  ],
+  "Goblet": [
+    {
+      "name": "Basic",
+      "geometryURL": "Items/Dining/goblet.json",
+      "textureURL": "Items/Dining/simple.svg",
+      "defaults": {
+        "Color": "#95a5a6",
+      },
+      "createdBy": "Clint Bellanger (modified)",
+      "license": "CC-BY 3.0",
+      'website': "http://opengameart.org/content/dinnerware"
+    }
+  ],
+  "Jug": [
+    {
+      "name": "Basic",
+      "geometryURL": "Items/Dining/jug.json",
+      "textureURL": "Items/Dining/simple.svg",
+      "defaults": {
+        "Color": "#f39c12",
+      },
+      "createdBy": "Clint Bellanger (modified)",
+      "license": "CC-BY 3.0",
+      'website': "http://opengameart.org/content/dinnerware"
+    }
+  ],
+  "Knife": [
+    {
+      "name": "Basic",
+      "geometryURL": "Items/Dining/knife.json",
+      "textureURL": "Items/Dining/simple.svg",
+      "defaults": {
+        "Color": "#95a5a6",
+      },
+      "createdBy": "Clint Bellanger (modified)",
+      "license": "CC-BY 3.0",
+      'website': "http://opengameart.org/content/dinnerware"
+    }
+  ],
+  "Pitcher": [
+    {
+      "name": "Basic",
+      "geometryURL": "Items/Dining/pitcher.json",
+      "textureURL": "Items/Dining/simple.svg",
+      "defaults": {
+        "Color": "#95a5a6",
+      },
+      "createdBy": "Clint Bellanger (modified)",
+      "license": "CC-BY 3.0",
+      'website': "http://opengameart.org/content/dinnerware"
+    }
+  ],
+  "Platter": [
+    {
+      "name": "Basic",
+      "geometryURL": "Items/Dining/platter.json",
+      "textureURL": "Items/Dining/simple.svg",
+      "defaults": {
+        "Color": "#d35400",
+      },
+      "createdBy": "Clint Bellanger (modified)",
+      "license": "CC-BY 3.0",
+      'website': "http://opengameart.org/content/dinnerware"
+    }
+  ],
+  "Spoon": [
+    {
+      "name": "Basic",
+      "geometryURL": "Items/Dining/spoon.json",
+      "textureURL": "Items/Dining/simple.svg",
+      "defaults": {
+        "Color": "#95a5a6",
+      },
+      "createdBy": "Clint Bellanger (modified)",
+      "license": "CC-BY 3.0",
+      'website': "http://opengameart.org/content/dinnerware"
+    }
+  ],
+  "Tankard": [
+    {
+      "name": "Basic",
+      "geometryURL": "Items/Dining/tankard.json",
+      "textureURL": "Items/Dining/simple.svg",
+      "defaults": {
+        "Color": "#95a5a6",
+      },
+      "createdBy": "Clint Bellanger (modified)",
+      "license": "CC-BY 3.0",
+      'website': "http://opengameart.org/content/dinnerware"
+    }
+  ],
+  "Weapon": [
+    {
+      "name": "Short Sword",
+      "geometryURL": "Accessories/Weapon/shortsword.json",
+      "textureURL": "Accessories/Weapon/weapon.svg",
+      "defaults": {
+        "Metal_Color": "#95a5a6",
+        "Handle_Color": "#2c3e50",
+        "Accent_Color": "#a0522d"
+      },
+      "createdBy": "Ralchire (modified)",
+      "license": "CC0",
+      'website': "http://opengameart.org/users/ralchire"
+    },
+    {
+      "name": "Scimitar",
+      "geometryURL": "Accessories/Weapon/scimitar.json",
+      "textureURL": "Accessories/Weapon/weapon.svg",
+      "defaults": {
+        "Metal_Color": "#95a5a6",
+        "Handle_Color": "#2c3e50",
+        "Accent_Color": "#a0522d"
+      },
+      "createdBy": "Ralchire (modified)",
+      "license": "CC0",
+      'website': "http://opengameart.org/users/ralchire"
+    },
+    {
+      "name": "Mace",
+      "geometryURL": "Accessories/Weapon/mace.json",
+      "textureURL": "Accessories/Weapon/weapon.svg",
+      "defaults": {
+        "Metal_Color": "#95a5a6",
+        "Handle_Color": "#2c3e50"
+      },
+      "createdBy": "Ralchire (modified)",
+      "license": "CC0",
+      'website': "http://opengameart.org/users/ralchire"
+    }
+  ],
+  "Shield": [
+    {
+      "name": "Basic",
+      "geometryURL": "Accessories/Shield/basic.json",
+      "textureURL": "Accessories/Shield/basic.svg",
+      "defaults": {
+        "Metal_Color": "#95a5a6",
+        "Primary_Color": "#ecf0f1",
+        "Secondary_Color": "#e74c3c"
+      },
+      "createdBy": "Chris Pollard",
+      "license": "CC0",
+      'website': "http://atlantia.tv"
+    },
+  ]
+};
+
+ATL.PremadeLibrary = [
+  {
+  "category": "Accessories",
+  "items": [
+    "Hat",
+    "Weapon",
+    "Shield"
+  ]},
+  {
+  "category": "Buildings",
+  "items": [
+    "Bridge"
+  ]},
+  {
+  "category": "Environment",
+  "items": [
+    "Tree",
+    "Flower",
+    "Mushroom",
+    "Crystal"
+  ]},
+  {
+  "category": "Vehicles",
+  "items": [
+    "Boat"
+  ]},
+  {
+  "category": "Furniture",
+  "items": [
+    "Chair",
+    "Table",
+    "Side Table",
+    "Bookcase",
+    "Bench",
+    "Crate"
+  ]},
+  {
+  "category": "Items",
+  "items": [
+    {
+      "category": "Dining",
+      "items": [
+        "Cup",
+        "Bowl",
+        "Plate",
+        "Fork",
+        "Goblet",
+        "Jug",
+        "Knife",
+        "Pitcher",
+        "Platter",
+        "Spoon",
+        "Tankard"
+      ]
+    }
+  ]},
+];
